5ca0bc7175d76f097c54a60e759d52960941f11d
Luke Harmon <lukeh@uidaho.edu>
2014-05-14 21:52:32 -0700

    fix typo

--- R/disparity.R
+++ R/disparity.R
@@ -5,16 +5,16 @@ function(x, f1, f2, xrange=c(0,1))
 	for(i in 1:length(x)) {
 		if(x[i]>=xrange[1] & x[i]<=xrange[2]) {
 			if(i==1) {
-				lhs<-0  
+				lhs<-0
 			} else if(x[i-1]<xrange[1]) {
 				lhs<-xrange[1]
 			} else lhs<-x[i-1];
-			if(i==length(x)) { 
+			if(i==length(x)) {
 				rhs<-x[i]
 			} else if(x[i+1]>xrange[2]) {
-				rhs<-xrange[2]; 
+				rhs<-xrange[2];
 			} else rhs<-x[i+1];
-			a<-a+(f2[i]-f1[i])*(rhs-lhs)/2;	
+			a<-a+(f2[i]-f1[i])*(rhs-lhs)/2;
 		} else if(i!=1) if(x[i-1]>=xrange[1] & x[i-1]<=xrange[2]) {
 			y1<-f1[i-1]+(f1[i]-f1[i-1])*(xrange[2]-x[i-1])/(x[i]-x[i-1])
 			y2<-f2[i-1]+(f2[i]-f2[i-1])*(xrange[2]-x[i-1])/(x[i]-x[i-1])
@@ -22,7 +22,7 @@ function(x, f1, f2, xrange=c(0,1))
 		} else if(i!=length(x)) if(x[i+1]>=xrange[1] & x[i+1]<=xrange[2]) {
 			y1<-f1[i]+(f1[i+1]-f1[i])*(xrange[1]-x[i])/(x[i+1]-x[i])
 			y2<-f2[i]+(f2[i+1]-f2[i])*(xrange[1]-x[i])/(x[i+1]-x[i])
-			
+
 			a<-a+(y2-y1)*(x[i+1]-xrange[1])/2;
 		}
 	}
@@ -32,7 +32,7 @@ function(x, f1, f2, xrange=c(0,1))
 
 
 disparity <- function(phy=NULL, data, index=c("avg.sq", "avg.manhattan", "num.states")){
-	
+
 	if(!is.null(phy)){
 		if (!"phylo"%in%class(phy)) stop("Supply 'phy' as a 'phylo' object")
 		td<-treedata(phy, data)
@@ -42,25 +42,25 @@ disparity <- function(phy=NULL, data, index=c("avg.sq", "avg.manhattan", "num.st
 		nb.tip <- length(td$phy$tip.label)
 		nb.node <- td$phy$Nnode
 		result<-numeric()
-		
+
 		for(i in 1:nb.node) {
 			l<-desc[[nb.tip+i]]
 			d<-td$data[phy$tip.label[l],]
 			result[i]<-.disparity(d, index)
 		}
 		names(result)=nb.tip+1:nb.node
-		return(result)	  	
-		
+		return(result)
+
 	} else {
 		return(.disparity(data, index))
 	}
-	
+
 }
 
 
 .disparity <- function(data, index=c("avg.sq", "avg.manhattan", "num.states")){
 	disp=match.arg(index, c("avg.sq", "avg.manhattan", "num.states"))
-	
+
 	if(disp=="avg.sq") {
 		d<-dist(data, method="euclidean")^2
 		r<-mean(d)
@@ -88,7 +88,7 @@ ratematrix=function(phy, dat){
 function(phy, data)
 {
 	td<-treedata(phy, data, sort=TRUE)
-	
+
 	f<-function(x) pic(x, td$phy)
 	ic<-apply(td$data, 2, function(x) {
 			  names(x)=rownames(td$data)
@@ -102,15 +102,15 @@ function(phy, data)
 .dtt <-
 function(phy, data, disp=c("avg.sq", "avg.manhattan", "num.states")){
 	disp=match.arg(disp, c("avg.sq", "avg.manhattan", "num.states"))
-	
+
 	phy$node.label<-NULL
 	td<-treedata(phy, data)
 	phy2<-td$phy
 	phy<-new2old.phylo(td$phy)
-	
+
 	result<-numeric()
-	
-	
+
+
 	node.depth<-branching.times(phy2);
 	stem.depth<-numeric();
 	stem.depth[1]<-node.depth[1];
@@ -118,7 +118,7 @@ function(phy, data, disp=c("avg.sq", "avg.manhattan", "num.states")){
 		anc<-which(as.numeric(phy$edge[,2])==-i)
 		stem.depth[i]<-node.depth[names(node.depth)==phy2$edge[anc,1]]
 	}
-	
+
 	ltt<-sort(node.depth, decreasing=TRUE)
 	node.depth<-node.depth/max(ltt);
 	stem.depth<-stem.depth/max(ltt);
@@ -134,16 +134,16 @@ function(phy, data, disp=c("avg.sq", "avg.manhattan", "num.states")){
 		result[length(ltt)+1]<-0;
 		if(result[1]>0)
 		result<-result/result[1];
-		
+
 	} else {
 		if(length(dim(td$data))!=3)
 		stop("Error in data");
-		
+
 		for(i in 1:dim(td$data)[3]) {
 			pp<-as.matrix(td$data[,,i])
 			d<-disparity(phy2, pp, index=disp)
 			y<-numeric()
-			
+
 			y[1]<-d[1]
 			for(j in 2:length(ltt)) {
 				x<-d[stem.depth>=ltt[j-1]&node.depth<ltt[j-1]]
@@ -153,18 +153,18 @@ function(phy, data, disp=c("avg.sq", "avg.manhattan", "num.states")){
 			y[length(ltt)+1]<-0;
 			if(y[1]>0)
 			y<-y/y[1];
-			
+
 			result<-cbind(result, y)
 		}
 	}
-	
-	return(result);	
+
+	return(result);
 }
 
 .dtt.polygon=function(mat, t, alpha=0.05){
 	k=nrow(mat)
 	dd=c(alpha/2, 1-alpha/2)
-	
+
 	tmp=sapply(1:k, function(x) quantile(mat[x,], probs=dd, na.rm=TRUE))
 	yy=c(tmp[1,], rev(tmp[2,]))
 	xx=c(t, rev(t))
@@ -179,37 +179,37 @@ function(phy, data, disp=c("avg.sq", "avg.manhattan", "num.states")){
 dtt<-function(phy, data, index=c("avg.sq", "avg.manhattan", "num.states"), mdi.range=c(0,1), nsim=0, CI=0.95, plot=TRUE, calculateMDIp=TRUE){
 
 	disp=match.arg(index, c("avg.sq", "avg.manhattan", "num.states"))
-	
+
 	td<-treedata(phy, data)
-	
+
 	dtt.data<-.dtt(td$phy, td$data, disp=disp)
 	ltt<-sort(branching.times(td$phy), decreasing=TRUE)
 	ltt<-c(0, (max(ltt)-ltt)/max(ltt));
-	
+
 	s<-ratematrix(td$phy, td$data)
 	dtt.sims=NULL
 	MDI=NULL
-	
+
     ylim=c(range(pretty(dtt.data)))
 
 	if(is.numeric(nsim)){
 		if(nsim>0){
 			sims<-sim.char(td$phy, s, nsim)
-			
+
 			dtt.sims<-.dtt(td$phy, sims)
 			mean.sims<-apply(dtt.sims, 1, mean)
             median.sims<-apply(dtt.sims, 1, median)
 
-			
+
 			MDI<-unname(.area.between.curves(ltt, apply(dtt.sims, 1, median), dtt.data, sort(mdi.range)))
             names(MDI)=disp
 			colnames(dtt.sims)=NULL
-            
+
             yy=range(dtt.sims)
             ylim=range(c(ylim, yy))
 		}
 	}
-	
+
 	if(plot){
 		plot(ltt, dtt.data, xlab="relative time", ylab="disparity", ylim=ylim, bty="n", type="n")
 
@@ -221,11 +221,11 @@ dtt<-function(phy, data, index=c("avg.sq", "avg.manhattan", "num.states"), mdi.r
         lines(ltt, dtt.data, type="l", lwd=2)
 
 	}
-	
+
 	res=list(dtt=dtt.data, times=ltt, sim=dtt.sims, MDI=MDI)
 	drp=sapply(res, function(x) is.null(x))
 	if(any(drp)) res=res[-which(drp)]
-	
+
 	if(calculateMDIp) {
 		pVal<-getMDIp(res)
 		res<-c(res, MDIpVal=pVal)
@@ -234,10 +234,10 @@ dtt<-function(phy, data, index=c("avg.sq", "avg.manhattan", "num.states"), mdi.r
 }
 
 getMDIp<-function(dttRes) {
-	foo <- function(x) {
+	foo<-function(x) {
 		return(.area.between.curves(x= dttRes$times, f1=x, f2 = dttRes$dtt))
 	}
-	mdis <- apply(dttRes$sim, 1, foo)
-	pVal <- length(which(mdis>=0)) / length(mdis) 
-	return(pVal)
+	mdis<-apply(dttRes$sim, 1, foo)
+	pVal<-length(which(mdis>=0)) / length(mdis) 
+	return(pVal);
 }
